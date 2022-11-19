12b2f1238c3e0626d431e4e9d54b67de5539d8d4
Chenyakov3 <98740038+Chenyakov3@users.noreply.github.com>
2022-11-19 13:36:29 +0200

    fifth try

--- Hello.java
+++ Hello.java
@@ -1,7 +1,7 @@
 public class Hello{
 	public static void main(String[] args){
 		for(int i=1;i<=10;i++){
-			System.out.println("Hello World fourth try"+i);
+			System.out.println("Hello World fifth try"+i);
 		}
 	}
 }
