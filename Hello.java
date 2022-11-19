d5d03ed409394d6e73fcf5ecb8336c955bb2e68a
Chenyakov3 <98740038+Chenyakov3@users.noreply.github.com>
2022-11-19 13:46:03 +0200

    yes

--- Hello.java
+++ Hello.java
@@ -1,7 +1,7 @@
 public class Hello{
 	public static void main(String[] args){
 		for(int i=1;i<=10;i++){
-			System.out.println("Hello World sixth try"+i);
+			System.out.println("Hello World yes"+i);
 		}
 	}
 }
