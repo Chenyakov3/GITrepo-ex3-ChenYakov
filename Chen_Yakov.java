bce1c31f7fa10f6592b61875ad5bf070ad4e36f6
Chenyakov3 <98740038+Chenyakov3@users.noreply.github.com>
2022-11-19 13:51:49 +0200

    ok

--- Chen_Yakov.java
+++ Chen_Yakov.java
@@ -1,7 +1,7 @@
 public class Chen_Yakov{
 	public static void main(String[] args){
 		for(int i=1;i<=10;i++){
-			System.out.println("Chen Yakov no"+i);
+			System.out.println("Chen Yakov ok"+i);
 		}
 	}
 }
